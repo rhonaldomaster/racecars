e7d4624ad235ce0b1b26aaca8fa855bf9742b82d
rhonaldomaster <rhonaldomaster@gmail.com>
2016-03-16 11:34:19 -0500

    Actualización para movimiento de vehículos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,55 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+var racecars = (function () {
+  var redcar = {};
+  var bluecar = {};
+  var createCar = function (img,speed,className,keyAdvance) {
+    var $view = $("."+className);
+    $view.prop("src",img);
+    return {
+      image: img,
+      speed: speed,
+      keyAdvance: keyAdvance,
+      move: function () {
+        var mleft = ($view.css("margin-left"));
+        mleft = mleft.substring(0,mleft.length-2);
+        mleft *=1;
+        $view.css("margin-left",(mleft+speed)+"px");
+      },
+      $view: $view
+    };
+  };
+  var initializeCars = function() {
+    redcar = createCar("assets/car-red.png",25,"js-redcar",83);
+    bluecar = createCar("assets/car-blue.png",25,"js-bluecar",87);
+    setControls();
+  };
+  var setControls = function(){
+    $("body").off("keyup");
+    $("body").on("keyup",function(ev) {
+      if(ev.which==redcar.keyAdvance){
+        redcar.move();
+      }
+      else if(ev.which==bluecar.keyAdvance){
+        bluecar.move();
+      }
+      if(redcar.$view.css("margin-left")>bluecar.$view.css("margin-left")){
+        $(".js-raceResult").html("Va ganando rojo");
+      }
+      else if(redcar.$view.css("margin-left")<bluecar.$view.css("margin-left")){
+        $(".js-raceResult").html("Va ganando azul");
+      }
+      else {
+        $(".js-raceResult").html("");
+      }
+    });
+  };
+  return{
+    initializeCars: initializeCars,
+    setControls: setControls
+  }
+})();
+$(document).ready(function () {
+  racecars.initializeCars();
+});
