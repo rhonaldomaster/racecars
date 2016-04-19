492f28b6640094ff5aad8d500d9f5f76c244f38d
rhonaldomaster <rhonaldomaster@gmail.com>
2016-04-19 15:32:59 -0500

    Mejoras varias

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,22 +1,8 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
 var racecars = (function () {
   var redcar = {};
   var bluecar = {};
+  var $raceresult;
   var createCar = function (img,speed,className,keyAdvance) {
     var $view = $("."+className);
     $view.prop("src",img);
@@ -24,45 +10,74 @@ var racecars = (function () {
       image: img,
       speed: speed,
       keyAdvance: keyAdvance,
+      points: 0,
       move: function () {
         var mleft = ($view.css("margin-left"));
         mleft = mleft.substring(0,mleft.length-2);
         mleft *=1;
         $view.css("margin-left",(mleft+speed)+"px");
       },
+      addPoints: function () {
+        this.points += 1;
+      },
       $view: $view
     };
   };
-  var initializeCars = function() {
+  var init = function() {
     redcar = createCar("assets/car-red.png",25,"js-redcar",83);
     bluecar = createCar("assets/car-blue.png",25,"js-bluecar",87);
     setControls();
-  };
-  var setControls = function(){
-    $("body").off("keyup");
-    $("body").on("keyup",function(ev) {
-      if(ev.which==redcar.keyAdvance){
-        redcar.move();
-      }
-      else if(ev.which==bluecar.keyAdvance){
-        bluecar.move();
-      }
-      if(redcar.$view.css("margin-left")>bluecar.$view.css("margin-left")){
-        $(".js-raceResult").html("Va ganando rojo");
-      }
-      else if(redcar.$view.css("margin-left")<bluecar.$view.css("margin-left")){
-        $(".js-raceResult").html("Va ganando azul");
-      }
-      else {
-        $(".js-raceResult").html("");
-      }
+    $('.js-btn-restart').off('click').on('click',function () {
+      resetGame();
     });
   };
+  var setControls = function(){
+    $("body").off("keyup").on("keyup",function(ev) {
+      $raceresult = $(".js-raceResult");
+      if(ev.which==redcar.keyAdvance) redcar.move();
+      else if(ev.which==bluecar.keyAdvance) bluecar.move();
+      if(redcar.$view.css("margin-left")>bluecar.$view.css("margin-left")) $raceresult.html("Va ganando rojo");
+      else if(redcar.$view.css("margin-left")<bluecar.$view.css("margin-left")) $raceresult.html("Va ganando azul");
+      else $raceresult.html("");
+      reviewRace();
+    });
+  };
+  var reviewRace = function () {
+    var tam = pxToNumber($('.js-road').css("width")) - pxToNumber($('.js-goal').css("height")) - 25;
+    var posr = pxToNumber(redcar.$view.css("margin-left")) + pxToNumber(redcar.$view.css("width"));
+    var posb = pxToNumber(bluecar.$view.css("margin-left")) + pxToNumber(bluecar.$view.css("width"));
+    if(posr>=tam || posb>=tam){
+      if(posr>=tam){
+        $raceresult.html("Gana rojo");
+        redcar.addPoints();
+      }
+      if(posb>=tam){
+        $raceresult.html("Gana azul");
+        bluecar.addPoints();
+      }
+      $("body").off("keyup");
+      viewResults();
+    }
+  };
+  var pxToNumber = function (px) {
+    var num = px.substring(0,px.length-2);
+    num *= 1;
+    return num;
+  };
+  var resetGame = function() {
+    redcar.$view.css("margin-left",0);
+    bluecar.$view.css("margin-left",0);
+    setControls();
+    $raceresult.html("");
+  };
+  var viewResults = function () {
+    $('.js-wins-blue').html(bluecar.points);
+    $('.js-wins-red').html(redcar.points);
+  };
   return{
-    initializeCars: initializeCars,
-    setControls: setControls
-  }
+    init: init
+  };
 })();
 $(document).ready(function () {
-  racecars.initializeCars();
+  racecars.init();
 });
